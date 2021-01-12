d027dca6bfc9e07ea98cc90799a57c14f02fe638
a98985240 <33692680+a98985240@users.noreply.github.com>
2021-01-13 04:16:41 +0800

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int times;
+    scanf("%d",&times);
+    for(int i=0;i<times;i++)
+    {
+        char x;
+        int a_real,a_ima,b_real,b_ima;
+        int real,ima;
+        scanf(" %c %d %d %d %d",&x,&a_real,&a_ima,&b_real,&b_ima);
+        if(x=='-')
+        {
+            real=a_real-b_real;
+            ima=a_ima-b_ima;
+        }
+        else if(x=='+')
+        {
+            real=a_real+b_real;
+            ima=a_ima+b_ima;
+        }
+        else if(x=='*')
+        {
+            real=a_real*b_real-a_ima*b_ima;
+            ima=a_real*b_ima+a_ima*b_real;
+        }
+        printf("%d %d\n",real,ima);
+    }
+}
