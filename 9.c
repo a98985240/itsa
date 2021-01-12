dce2c2b156febe5916e8a9246fe82be43b535a8d
a98985240 <33692680+a98985240@users.noreply.github.com>
2021-01-13 04:18:58 +0800

    Add files via upload

---
+++ 9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int input;
+    while (scanf("%d", &input) != EOF)
+    {
+        int sum = 0;
+        for (int i = 1; i <= input; i++)
+        {
+            if (i % 3 == 0)
+            {
+                sum += i;
+            }
+        }
+        printf("%d\n",sum);
+    }
+}
