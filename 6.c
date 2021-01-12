eb0a2407f23613f627bf05d82c346dc86b9e2991
a98985240 <33692680+a98985240@users.noreply.github.com>
2021-01-13 04:15:19 +0800

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int input;
+    while(scanf("%d",&input)!=EOF)
+    {
+        if(input>=3&&input<=5)
+        {
+            printf("Spring\n");
+        }
+        else if(input>=6&&input<=8)
+        {
+            printf("Summer\n");
+        }
+        else if(input>=9&&input<=11)
+        {
+            printf("Autumn\n");
+        }
+        else if(input==12||input<=2)
+        {
+            printf("Winter\n");
+        }
+    }
+}
